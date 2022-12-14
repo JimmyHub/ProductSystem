77f038e2599b492ec3f3c15bcbfae9e80769e5ef
JimmyHub <j29241919@gmail.com>
2022-12-14 18:25:09 +0800

    feature: 從外部直接匯入庫存資料

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,18 +6,10 @@ Vue.use(Router)
 
 export default new Router({
      routes:[
-        // 總表
-        { 
+        {
           name:'index',
           path:'/index',
           component:history
-        },
-        // 歷史表
-        // { 
-        //   name:'history',
-        //   path:'/history',
-        //   component:history
-        // }
-
+        }
         ]
 })
